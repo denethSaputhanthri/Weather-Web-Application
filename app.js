28b501aedecc9e0046a94f357778637641b3b80a
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2025-12-21 21:50:07 +0530

    style: enhance responsive design and improve font sizes across breakpoints

--- app.js
+++ app.js
@@ -41,9 +41,4 @@ function Search() {
     }
   });
 }
-  // Handle window resize
-  window.addEventListener("resize", () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  });
+
